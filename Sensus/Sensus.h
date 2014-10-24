23f9473e1a4ea786671145cf2c8544138b28c864
DAloG <dalog@me.com>
2014-10-24 21:53:22 +0300

    Initial Commit

---
+++ Sensus/Sensus.h
@@ -0,0 +1,13 @@
+//
+//  Sensus.h
+//  Sensus
+//
+//  Created by Алексей Демедецкий on 24.10.14.
+//  Copyright (c) 2014 DAloG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sensus : NSObject
+
+@end
---
+++ Sensus/Sensus.m
@@ -0,0 +1,13 @@
+//
+//  Sensus.m
+//  Sensus
+//
+//  Created by Алексей Демедецкий on 24.10.14.
+//  Copyright (c) 2014 DAloG. All rights reserved.
+//
+
+#import "Sensus.h"
+
+@implementation Sensus
+
+@end
